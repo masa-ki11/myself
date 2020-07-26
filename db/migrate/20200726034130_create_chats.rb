88a456cbd06ff9ccf93d9cd15b77f4d8ee0cea31
masa-ki11 <67939220+masa-ki11@users.noreply.github.com>
2020-07-26 12:53:30 +0900

    done

--- db/migrate/20200726034130_create_chats.rb
+++ db/migrate/20200726034130_create_chats.rb
@@ -1,7 +1,7 @@
 class CreateChats < ActiveRecord::Migration[6.0]
   def change
     create_table :chats do |t|
-
+      t.string :comment
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_080145) do
+ActiveRecord::Schema.define(version: 2020_07_26_034544) do
 
   create_table "chats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -23,8 +24,12 @@ ActiveRecord::Schema.define(version: 2020_07_25_080145) do
   end
 
   create_table "user_rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_user_rooms_on_room_id"
+    t.index ["user_id"], name: "index_user_rooms_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -43,4 +48,6 @@ ActiveRecord::Schema.define(version: 2020_07_25_080145) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_rooms", "rooms"
+  add_foreign_key "user_rooms", "users"
 end
