927436ce8803c7ac6448d0cade03f274b6be7e4b
masa-ki11 <67939220+masa-ki11@users.noreply.github.com>
2020-07-28 14:23:52 +0900

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :create, :new, :edit, :update] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
 end
