63852145181efa4472ccca0894432bc9a592b415
masa-ki11 <67939220+masa-ki11@users.noreply.github.com>
2020-08-30 03:57:02 +0900

    a

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
   
   def default_url
-    asset_path('board_placeholder.png')
+    'board_placeholder.png'
   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
